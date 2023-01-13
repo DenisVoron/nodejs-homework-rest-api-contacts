d48855cc5a03564d869c52989e0f485b6cb79466
DenisVoron <Denis.voronko@gmail.com>
2023-01-13 16:48:04 +0200

    add final

--- src/controllers/contacts/getContacts.js
+++ src/controllers/contacts/getContacts.js
@@ -1,20 +1,12 @@
 const Contact = require('../../models/contacts')
 
-
 const getContacts = async (req, res) => {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 10, favorite } = req.query;
-    console.log(typeof favorite)
+    const { page = 1, limit = 10 } = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({ owner }, '-createdAt -updatedAt')
-        .skip(skip)
-        .limit(limit)
-        .sort({ favorite: true, type: 'Boolean' })
+    const result = await Contact.find({ owner }, '-createdAt -updatedAt', { skip, limit })
         .populate('owner', "email");
     res.status(200).json(result);
 }
 
 module.exports = getContacts;
-
-/** .skip(page)
-        .limit(limit) { favorite: { $eq: favorite } } */
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,7 +2,7 @@ const {
     postValidation,
     putValidation,
     patchValidation
-} = require('./walidationMiddleware');
+} = require('./validationMiddleware');
 
 const {
     registValidation,
